fa759159022ae7cd4130035a7f6d1d9f7a51a7f8
tommy <shchuplov@gmail.com>
2012-11-19 23:15:45 +0300

    ok

--- irq.py
+++ irq.py
@@ -6,7 +6,7 @@
 # and open the template in the editor.
 import os
 
-__author__ = "Maksim Shchuplov shchuplov@gmail.com"
+__author__ = "MaksimShchuplov shchuplov@gmail.com"
 __date__ = "$06.06.2012 5:10:22$"
 
 if __name__ == "__main__":
